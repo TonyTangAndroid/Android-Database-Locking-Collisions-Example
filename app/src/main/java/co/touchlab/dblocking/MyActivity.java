8e76bd6e8dc7d28b72f671a6d36250e2e18c8833
TonyTang <tonytang@uber.com>
2022-11-21 23:18:54 -0800

    Format code.

--- app/src/main/java/co/touchlab/dblocking/MyActivity.java
+++ app/src/main/java/co/touchlab/dblocking/MyActivity.java
@@ -11,304 +11,241 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import co.touchlab.R;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MyActivity extends Activity
-{
-    private AtomicInteger allCount = new AtomicInteger();
-    private Handler uiHandler;
+@SuppressWarnings("deprecation")
+public class MyActivity extends Activity {
 
+  private final AtomicInteger allCount = new AtomicInteger();
+  private Handler uiHandler;
+
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    uiHandler = new Handler();
+
+    findViewById(R.id.oneHelper)
+        .setOnClickListener(view -> {
+          allCount.set(0);
+          final List<DbInsertThread> allThreads = new ArrayList<>();
+
+          DatabaseHelper helper = new DatabaseHelper(MyActivity.this);
+
+          for (int i = 0; i < 4; i++) {
+            allThreads.add(new DbInsertThread(helper, 100));
+          }
+
+          runAllThreads(allThreads);
+        });
+
+    findViewById(R.id.manyHelpers)
+        .setOnClickListener(view -> {
+          allCount.set(0);
+          final List<DbInsertThread> allThreads = new ArrayList<>();
+
+          for (int i = 0; i < 4; i++) {
+            DatabaseHelper helper = new DatabaseHelper(MyActivity.this);
+            allThreads.add(new DbInsertThread(helper, 100));
+          }
+
+          runAllThreads(allThreads);
+        });
+
+    findViewById(R.id.testTransactionIsolation).setOnClickListener(view -> {
+      DatabaseHelper helper = new DatabaseHelper(MyActivity.this);
+      new SlowInsertThread(helper).start();
+      new FastSelectThread(helper).start();
+    });
+
+    findViewById(R.id.noTrans).setOnClickListener(view -> runNoTrans());
+
+    findViewById(R.id.trans).setOnClickListener(view -> runTrans());
+  }
+
+  @SuppressWarnings("unchecked")
+  private void runTrans() {
+    new AsyncTask() {
+
+      @Override
+      protected Object doInBackground(Object... objects) {
+        DatabaseHelper instance = DatabaseHelper.getInstance(MyActivity.this);
+
+        SQLiteDatabase writableDatabase = instance.getWritableDatabase();
+
+        long start = System.currentTimeMillis();
+        writableDatabase.beginTransaction();
+        insertTest(instance);
+        writableDatabase.setTransactionSuccessful();
+        writableDatabase.endTransaction();
+
+        return start;
+      }
+
+      @Override
+      protected void onPostExecute(Object o) {
+        showTime((Long) o, DatabaseHelper.getInstance(MyActivity.this));
+      }
+    }.execute();
+
+
+  }
+
+  @SuppressWarnings("unchecked")
+  private void runNoTrans() {
+    new AsyncTask() {
+
+      @Override
+      protected Object doInBackground(Object... objects) {
+        DatabaseHelper instance = DatabaseHelper.getInstance(MyActivity.this);
+        long start = System.currentTimeMillis();
+        insertTest(instance);
+
+        return start;
+      }
+
+      @Override
+      protected void onPostExecute(Object o) {
+        showTime((Long) o, DatabaseHelper.getInstance(MyActivity.this));
+      }
+    }.execute();
+  }
+
+  private void showTime(long start, DatabaseHelper instance) {
+    long time = System.currentTimeMillis() - start;
+    ((TextView) findViewById(R.id.timeOut)).setText(
+        "Total rows: " + instance.countSessions() + "/time: " + Long.toString(time));
+  }
+
+  private void insertTest(DatabaseHelper helper) {
+
+    int numberOfInserts = Integer.parseInt(
+        ((EditText) findViewById(R.id.numberOfInserts)).getText().toString());
+    while (numberOfInserts > 0) {
+      helper.createSession("Count: " + numberOfInserts);
+      numberOfInserts--;
+    }
+
+  }
+
+  static class SlowInsertThread extends Thread {
+
+    private final DatabaseHelper helper;
+    private Handler handler;
+
+    SlowInsertThread(DatabaseHelper helper) {
+      this.helper = helper;
+      handler = new Handler();
+    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        uiHandler = new Handler();
+    public void run() {
+      int count = 0;
+      while (count < 10) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.beginTransaction();
+        final ContentValues contentValues = new ContentValues();
 
-        findViewById(R.id.oneHelper)
-                .setOnClickListener(new View.OnClickListener()
-                {
+        contentValues.put("description", "asdlkfj");
 
-                    public void onClick(View view)
-                    {
-                        allCount.set(0);
-                        final List<DbInsertThread> allThreads = new ArrayList<DbInsertThread>();
+        Log.i(getClass().getName(), "insert");
+        db.insertOrThrow("session", null, contentValues);
+        Log.i(getClass().getName(), "start wait");
+        try {
+          Thread.sleep(5000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        Log.i(getClass().getName(), "end wait");
+        db.endTransaction();
 
-                        DatabaseHelper helper = new DatabaseHelper(MyActivity.this);
+        count++;
+      }
+    }
+  }
 
-                        for(int i=0; i<4; i++)
-                        {
-                            allThreads.add(new DbInsertThread(helper, 100));
-                        }
+  static class FastSelectThread extends Thread {
 
-                        runAllThreads(allThreads);
-                    }
-                });
+    private final DatabaseHelper helper;
+    private Handler handler;
 
-        findViewById(R.id.manyHelpers)
-                .setOnClickListener(new View.OnClickListener()
-                {
+    FastSelectThread(DatabaseHelper helper) {
+      this.helper = helper;
+      handler = new Handler();
+    }
 
-                    public void onClick(View view)
-                    {
-                        allCount.set(0);
-                        final List<DbInsertThread> allThreads = new ArrayList<DbInsertThread>();
+    @Override
+    public void run() {
+      int count = 0;
+      while (count < 100) {
+        helper.loadAllSessions();
+        Log.i(getClass().getName(), "selected");
 
-                        for(int i=0; i<4; i++)
-                        {
-                            DatabaseHelper helper = new DatabaseHelper(MyActivity.this);
-                            allThreads.add(new DbInsertThread(helper, 100));
-                        }
+        Log.i(getClass().getName(), "start wait");
+        try {
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        Log.i(getClass().getName(), "end wait");
 
-                        runAllThreads(allThreads);
-                    }
-                });
-        
-        findViewById(R.id.testTransactionIsolation).setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                DatabaseHelper helper = new DatabaseHelper(MyActivity.this);
-                new SlowInsertThread(helper).start();
-                new FastSelectThread(helper).start();
-            }
+        count++;
+      }
+    }
+  }
+
+  private void runAllThreads(final List<DbInsertThread> allThreads) {
+    new Thread(new Runnable() {
+      public void run() {
+        for (DbInsertThread allThread : allThreads) {
+          allThread.start();
+        }
+        for (DbInsertThread thread : allThreads) {
+          try {
+            thread.join();
+          } catch (InterruptedException e) {
+          }
+        }
+
+        uiHandler.post(new Runnable() {
+          public void run() {
+            ((TextView) findViewById(R.id.results)).setText("All thread Inserted " + allCount.get());
+          }
         });
-        
-        findViewById(R.id.noTrans).setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                runNoTrans();
-            }
-        });
+      }
+    }).start();
+  }
 
-        findViewById(R.id.trans).setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                runTrans();
-            }
-        });
+  class DbInsertThread extends Thread {
+
+    private final DatabaseHelper helper;
+    private final int runCount;
+
+    DbInsertThread(DatabaseHelper helper, int runCount) {
+      this.helper = helper;
+      this.runCount = runCount;
     }
 
-    @SuppressWarnings("unchecked")
-    private void runTrans()
-    {
-        new AsyncTask(){
-
-            @Override
-            protected Object doInBackground(Object... objects)
-            {
-                DatabaseHelper instance = DatabaseHelper.getInstance(MyActivity.this);
-
-                SQLiteDatabase writableDatabase = instance.getWritableDatabase();
-
-                long start = System.currentTimeMillis();
-                writableDatabase.beginTransaction();
-                insertTest(instance);
-                writableDatabase.setTransactionSuccessful();
-                writableDatabase.endTransaction();
-
-                return start;
-            }
-
-            @Override
-            protected void onPostExecute(Object o)
-            {
-                showTime((Long)o, DatabaseHelper.getInstance(MyActivity.this));
-            }
-        }.execute();
-
-
-    }
-    
-    @SuppressWarnings("unchecked")
-    private void runNoTrans()
-    {
-        new AsyncTask(){
-
-            @Override
-            protected Object doInBackground(Object... objects)
-            {
-                DatabaseHelper instance = DatabaseHelper.getInstance(MyActivity.this);
-                long start = System.currentTimeMillis();
-                insertTest(instance);
-
-                return start;
-            }
-
-            @Override
-            protected void onPostExecute(Object o)
-            {
-                showTime((Long)o, DatabaseHelper.getInstance(MyActivity.this));
-            }
-        }.execute();
-    }
-
-    private void showTime(long start, DatabaseHelper instance)
-    {
-        long time = System.currentTimeMillis() - start;
-        ((TextView)findViewById(R.id.timeOut)).setText("Total rows: "+ instance.countSessions() +"/time: "+ Long.toString(time));
-    }
-
-    private void insertTest(DatabaseHelper helper)
-    {
-        
-        int numberOfInserts = Integer.parseInt(((EditText) findViewById(R.id.numberOfInserts)).getText().toString());
-        while(numberOfInserts > 0)
-        {
-            helper.createSession("Count: "+ numberOfInserts);
-            numberOfInserts--;
-        }
-        
-    }
-
-    class SlowInsertThread extends Thread
-    {
-        private DatabaseHelper helper;
-        private Handler handler;
-
-        SlowInsertThread(DatabaseHelper helper)
-        {
-            this.helper = helper;
-            handler = new Handler();
+    @Override
+    public void run() {
+      Random random = new Random();
+      for (int i = 0; i < runCount; i++) {
+        if (i % 10 == 0) {
+          Log.i("asdf", "Writing");
         }
 
-        @Override
-        public void run()
-        {
-            int count = 0;
-            while(count < 10)
-            {
-                SQLiteDatabase db = helper.getWritableDatabase();
-                db.beginTransaction();
-                final ContentValues contentValues = new ContentValues();
-
-                contentValues.put("description", "asdlkfj");
-
-                Log.i(getClass().getName(), "insert");
-                db.insertOrThrow("session", null, contentValues);
-                Log.i(getClass().getName(), "start wait");
-                try
-                {
-                    Thread.sleep(5000);
-                }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-                Log.i(getClass().getName(), "end wait");
-                db.endTransaction();
-
-                count++;
-            }
+        try {
+          helper.createSession("heyo - " + random.nextInt(12345678));
+          allCount.incrementAndGet();
+        } catch (Exception e) {
+          //Insert failed!!!
+          Log.e("asdf", "Insert failed!!!");
         }
+      }
     }
-
-    class FastSelectThread extends Thread
-    {
-        private DatabaseHelper helper;
-        private Handler handler;
-
-        FastSelectThread(DatabaseHelper helper)
-        {
-            this.helper = helper;
-            handler = new Handler();
-        }
-
-        @Override
-        public void run()
-        {
-            int count = 0;
-            while(count < 100)
-            {
-                helper.loadAllSessions();
-                Log.i(getClass().getName(), "selected");
-
-                Log.i(getClass().getName(), "start wait");
-                try
-                {
-                    Thread.sleep(500);
-                }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-                Log.i(getClass().getName(), "end wait");
-
-                count++;
-            }
-        }
-    }
-
-    private void runAllThreads(final List<DbInsertThread> allThreads)
-    {
-        new Thread(new Runnable()
-        {
-            public void run()
-            {
-                for (DbInsertThread allThread : allThreads)
-                {
-                    allThread.start();
-                }
-                for (DbInsertThread thread : allThreads)
-                {
-                    try
-                    {
-                        thread.join();
-                    }
-                    catch (InterruptedException e)
-                    {
-                    }
-                }
-
-                uiHandler.post(new Runnable()
-                {
-                    public void run()
-                    {
-                        ((TextView)findViewById(R.id.results)).setText("Inserted "+ allCount.get());
-                    }
-                });
-            }
-        }).start();
-    }
-
-    class DbInsertThread extends Thread
-    {
-        private DatabaseHelper helper;
-        private int runCount;
-
-        DbInsertThread(DatabaseHelper helper, int runCount)
-        {
-            this.helper = helper;
-            this.runCount = runCount;
-        }
-
-        @Override
-        public void run()
-        {
-            Random random = new Random();
-            for(int i=0; i<runCount; i++)
-            {
-                if(i % 10 == 0)
-                    Log.i("asdf", "Writing");
-
-                try
-                {
-                    helper.createSession("heyo - "+ random.nextInt(12345678));
-                    allCount.incrementAndGet();
-                }
-                catch (Exception e)
-                {
-                    //Insert failed!!!
-                    Log.e("asdf", "Insert failed!!!");
-                }
-            }
-        }
-    }
+  }
 }
